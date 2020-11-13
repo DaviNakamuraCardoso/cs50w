f36fe8d42b46ef45257918e8aaacb641191c7a69
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-11-13 20:10:30 -0300

    Lecture 08: Fishing examples

--- 08_Security/phising/bank/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    if request.method == "POST":
-        id = request.POST["onlineId1"]
-        password = request.POST["passcode1"]
-
-    return render(request, "index.html")
