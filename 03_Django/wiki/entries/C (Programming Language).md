afec745eede41f7b294c3ee6ebaff9ac042e5e4c
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-09-23 22:12:38 -0300

    Removing useless files

---
+++ 03_Django/wiki/entries/C (Programming Language).md
@@ -0,0 +1,2 @@
+# C (Programming Language)
+C is a general-purpose programming language created in 1970 by Dennis Ritchie.
--- 08_Security/ping.c
+++ 08_Security/ping.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <unistd.h>
+#include <curl/curl.h>
 
-#define THREADS 20
+
+#define TARGET "https://en.wikipedia.org/wiki/C_(programming_language)"
 
 
-static void* ping(void* v)
+static void ping(void)
 {
     for (int i = 0; i < 5000; i++)
     {
-        system("ping p4ed.com");
+        CURL* curl = curl_easy_init();
+        if (curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, TARGET); 
+
+            res = curl_easy_perform(curl); 
+            curl_easy_cleanup(curl);
+            sleep(1);
+        }
+
     }
 }
 
 int main(void)
 {
-    pthread_t p[THREADS];
 
-    for (int i = 0; i < THREADS; i++)
-    {
-        pthread_create(&p[i], NULL, ping, NULL);
-    }
+    while (1) ping();
+    return 0;
 
 }
